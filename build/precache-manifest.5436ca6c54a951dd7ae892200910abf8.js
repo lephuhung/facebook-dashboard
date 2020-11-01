14d881e861073991545fe90633fe3ac64a1181ad
lephuhung <lephuhung77@gmail.com>
2020-11-01 22:01:04 +0700

    ok

---
+++ build/precache-manifest.5436ca6c54a951dd7ae892200910abf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0dc0778de84b798b7d4b39a038fd8d",
+    "url": "/facebook-dashboard/index.html"
+  },
+  {
+    "revision": "de4d874481c9da363a67",
+    "url": "/facebook-dashboard/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "4cb0d55b35de9c040490",
+    "url": "/facebook-dashboard/static/css/main.d2646a32.chunk.css"
+  },
+  {
+    "revision": "de4d874481c9da363a67",
+    "url": "/facebook-dashboard/static/js/2.d3bfab22.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/facebook-dashboard/static/js/2.d3bfab22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cb0d55b35de9c040490",
+    "url": "/facebook-dashboard/static/js/main.bc875657.chunk.js"
+  },
+  {
+    "revision": "b4e7bf851a2a8438ee9f",
+    "url": "/facebook-dashboard/static/js/runtime-main.990e496b.js"
+  }
+]);
